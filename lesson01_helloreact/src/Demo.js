c4217199d126b2860e62f52b842eeb0e63f2b532
khoalun <buikhoa793@gmail.com>
2020-08-14 15:34:19 +0300

    first commit

---
+++ lesson01_helloreact/src/Demo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+    render() {
+
+
+        return (
+            <div>
+                <h1> Hello Reactjs</h1>
+                <p>Chao mung den khoa hoc lap trinh react</p>
+            </div>
+        );
+    }
+}
+
+export default Demo;
